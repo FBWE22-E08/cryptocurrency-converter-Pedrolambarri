c35958fc7c8bfa16b597897c2c78495a5cddbf0b
Pedro Lambarri <plambarri@gmail.com>
2023-01-31 17:11:06 +0100

    cryoti-currency-converter

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
-// import "./css/style.css"; //importing css
+import "./css/style.css"; //importing css
 
+const form = document.querySelector("#conversion-form");
+const cryptoCurrency = document.querySelector('.cryptoSelect');
+const inputAmount = document.querySelector('#inputAmount');
+const currency = document.querySelector('#dropdown2');
+const btn = document.querySelector('.btn');
+let output = document.querySelector('#output');
+
+async function currencyFetch(e) {
+    e.preventDefault();
+    try {
+        let response = await fetch(`https://api.coinconvert.net/convert/${cryptoCurrency.value}/${currency.value}?amount=${inputAmount.value}`);
+        let data = await response.json();
+       
+        console.log(data);
+      
+        output.value = Object.values(data)[2];
+       
+
+    }
+        catch (err) {
+            console.log(err);
+        }
+
+}
+
+form.addEventListener("submit", currencyFetch);
